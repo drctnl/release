aca9f28c784b306e8140e83c3d9e3870d0b5f9ce
CI/CD <drctnl@users.noreply.github.com>
2025-02-21 13:49:28 +0000

    Updated distribution of action.

--- dist/index.js
+++ dist/index.js
@@ -23919,12 +23919,19 @@ async function updateOrCreateRelease(version, prefix, preReleaseIdentifier, gith
     }
     return tagName;
   }
+  const notes = await kit.rest.repos.generateReleaseNotes({
+    tag_name: tagName,
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    target_commitish: github.context.sha
+  });
   if (!release) {
     core.info(`Creating release ${releaseName}`);
     const response = await kit.rest.repos.createRelease({
-      name: releaseName,
+      name: notes.data.name,
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
+      body: notes.data.body,
       tag_name: tagName,
       prerelease: !!preReleaseIdentifier,
       draft: true,
@@ -23934,6 +23941,8 @@ async function updateOrCreateRelease(version, prefix, preReleaseIdentifier, gith
   } else {
     core.info(`Updating release ${releaseName}`);
     await kit.rest.repos.updateRelease({
+      name: notes.data.name,
+      body: notes.data.body,
       release_id: release.id,
       owner: github.context.repo.owner,
       prerelease: !!preReleaseIdentifier,
