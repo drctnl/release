c4005fcc8d254f18b26986b2a612b9709ddce891
Moritz Fechtner <moritz.fechtner@drctnl.com>
2025-02-21 13:40:24 +0100

    Enhance CI/CD workflow and release management
    - Updated cicd.yml to include GitHub token for release action
    - Improved README with detailed release process and assumptions
    - Modified action.yaml to add pre-release identifier input
    - Refined updateOrCreateRelease function to handle pre-release tags and error messages for existing releases

--- README.md
+++ README.md
@@ -7,6 +7,17 @@ Workflow:
 
 1. Get latest releases (limit is 30 currently)
 2. Check if release matching the version exists
-3. Fails if a matching release has been published
+3. Fails if a matching release has been published with different commit otherwise returns version (tag) and stops execution
 4. Updates any existing release by modifying the tag and target commit
 5. If none exist, it creates a draft
+6. Returns version (tag) with build identifier (commit) as suffix
+
+Assumptions:
+
+- versions follow the SemVer spec
+- every change contributes to a release
+- the repo defines the lifecycle of a release
+
+Todos:
+
+- clean up code - despaghettify
--- index.mjs
+++ index.mjs
@@ -32,11 +32,13 @@ async function readVersion(path) {
   return content.trim();
 }
 
-async function updateOrCreateRelease(version, prefix, githubToken) {
-  const releaseName = `${prefix}${version}`;
+async function updateOrCreateRelease(version, prefix, preReleaseIdentifier, githubToken) {
   const kit = github.getOctokit(githubToken, {
     "userAgent": "github-actions-release",
   });
+
+  const releaseName = `${prefix}${version}`;
+  let tagName = preReleaseIdentifier ? `${releaseName}-${preReleaseIdentifier}` : releaseName;
 
   const releases = (await kit.rest.repos.listReleases({
     owner: github.context.repo.owner,
@@ -45,10 +47,17 @@ async function updateOrCreateRelease(version, prefix, githubToken) {
     per_page: 30,
   }))?.data;
 
-  let release = releases.find((r) => r.name == releaseName);
+
+  let release = releases.find((r) => r.tag_name == tagName);
 
   if (release?.published_at) {
-    throw new Error(`Release ${releaseName} already published.`);
+    core.info(`Release ${releaseName} already published.`);
+
+    if (release.target_commitish != github.context.sha) {
+      throw new Error("Release already published with different commit.");
+    }
+
+    return tagName;
   }
 
   if (!release) {
@@ -58,7 +67,8 @@ async function updateOrCreateRelease(version, prefix, githubToken) {
       name: releaseName,
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
-      tag_name: releaseName,
+      tag_name: tagName,
+      prerelease: !!preReleaseIdentifier,
       draft: true,
       target_commitish: github.context.sha,
     });
@@ -69,15 +79,14 @@ async function updateOrCreateRelease(version, prefix, githubToken) {
     await kit.rest.repos.updateRelease({
       release_id: release.id,
       owner: github.context.repo.owner,
+      prerelease: !!preReleaseIdentifier,
       repo: github.context.repo.repo,
       tag_name: releaseName,
       target_commitish: github.context.sha,
     });
   }
 
-  if (release?.draft) {
-    return `${releaseName}+${github.context.sha}`;
-  }
 
-  return releaseName;
+
+  return `${tagName}+${github.context.sha}`;
 }
